a1c00560e1485feff595b69007f88a9a8144031d
glebson1988 <glebson1988@yandex.ru>
2020-09-30 11:46:47 +0300

    moving the Results partial back into My portfolio

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,17 @@
 var app = angular.module('FinanceTrackerApp', [])
-                 .controller('stocksController', ['$scope', function ($scope){
+    .controller('stocksController', ['$scope', function ($scope) {
 
-                 }]);
+        $scope.stock = {};
+
+        $scope.lookup = function () {
+            if ($scope.ticker != undefined && $scope.ticker != '') {
+                $scope.stock = {
+                    symbol: 'FOO',
+                    name: 'Example.Corp.',
+                    last_price: '123.00'
+                }
+            } else {
+                $scope.stock = {};
+            }
+        }
+    }]);
